11265fb69bf6c2b5d6398cee48bcbf67d9e52f7c
ljs3536 <wlstjr320@naver.com>
2021-03-12 16:08:02 +0900

    hello-world!

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');  //설치한 express module을 불러와서 변수에 담음
 var app = express(); //express를 실행하여 app object를 초기화
 
-app.get('/', function(req, res){ // '/'위치에 'get'요청을 받는 경우,
-  res.send('Hello World!');     //"Hello World!"를 보낸다.
-});
+// app.get('/', function(req, res){ // '/'위치에 'get'요청을 받는 경우,
+//   res.send('Hello World!');     //"Hello World!"를 보낸다.
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 var port = 3000; //사용할 포트 번호를 port 변수에 넣음
 app.listen(port, function(){  // port변수를 이용하여 3000번 포트에 node.js 서버를 연결
